c24dd1c4644e20b78173cd83436ca85a07ea7b7b
rrayyhanep <rrayyhanep@gmail.com>
2026-01-07 08:58:54 +0000

    Success

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // The output property is removed to enable server-side rendering
 };
 
 export default nextConfig;
